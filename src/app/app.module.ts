4d74ec794507b509009414345e2d202ea11ff625
Frida <fridaahlstrom@outlook.com>
2021-11-10 17:43:12 +0000

    Util components added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,6 +11,11 @@ import { BodyComponent } from './components/body/body.component';
 import { HomeComponent } from './components/routes/home/home.component';
 import { HistoryComponent } from './components/routes/history/history.component';
 import { AccountComponent } from './components/routes/account/account.component';
+import { ButtonComponent } from './components/utils/button/button.component';
+import { TableComponent } from './components/utils/table/table.component';
+import { ImageComponent } from './components/utils/image/image.component';
+import { MapComponent } from './components/utils/map/map.component';
+import { TextareaComponent } from './components/utils/textarea/textarea.component';
 
 @NgModule({
   declarations: [
@@ -20,7 +25,12 @@ import { AccountComponent } from './components/routes/account/account.component'
     BodyComponent,
     HomeComponent,
     HistoryComponent,
-    AccountComponent
+    AccountComponent,
+    ButtonComponent,
+    TableComponent,
+    ImageComponent,
+    MapComponent,
+    TextareaComponent
   ],
   imports: [
     BrowserModule,
